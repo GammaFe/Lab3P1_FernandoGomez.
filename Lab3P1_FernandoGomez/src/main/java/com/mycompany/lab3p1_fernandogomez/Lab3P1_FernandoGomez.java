a6178b908bcb8a9bc29148abe22e17ed4fb2ace4
GammaFe <115840414+GammaFe@users.noreply.github.com>
2023-02-03 16:29:19 -0600

    Update Lab3P1_FernandoGomez.java

--- Lab3P1_FernandoGomez/src/main/java/com/mycompany/lab3p1_fernandogomez/Lab3P1_FernandoGomez.java
+++ Lab3P1_FernandoGomez/src/main/java/com/mycompany/lab3p1_fernandogomez/Lab3P1_FernandoGomez.java
@@ -21,57 +21,80 @@ public class Lab3P1_FernandoGomez {
             opcion = entrada.nextInt();
 
             switch (opcion) {
-                case 1:
+                case 1:// recuerda regresar a este ejercicio ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                     System.out.print("Ingrese el tamaño de la figura: ");
                     int tamaño = entrada.nextInt();
-                    // System.out.print("Ingrese el caracter para el traingulo superior: ");
-                    // String str = entrada.nextLine();
-                    // char carac = str.charAt(0);
+                    entrada = new Scanner(System.in);
+                    System.out.print("Ingrese el caracter para el traingulo superior: ");
+
+                    String str = entrada.nextLine();
                     int i;
-                        for (i = 0; i < tamaño; i++) {
-                            for (int  j=tamaño; j > i; j--) {
-                                System.out.print("A");
-                               
-                            }
-                            System.out.println("");
+                    for (i = 0; i < tamaño; i++) {
+                        for (int y = i; y >= 0; y--) {
+                            System.out.print(" ");
                         }
-                        break;
-                case 2:          
-                     System.out.print("Ingrese el límite de la sumatoria: ");
+                        for (int j = tamaño; j > i; j--) {
+                            System.out.print(str);
+
+                        }
+
+                        System.out.println("");
+                    }
+                    break;
+
+                case 2:
+                    entrada = new Scanner(System.in);
+                    System.out.print("Ingrese el límite de la sumatoria: ");
                     int num = entrada.nextInt();
                     int cont = 0;
                     float a = 0,
                      b = 0,
                      resp = 0,
                      res,
-                      resp2 = 0,
-                            resp3 =0;
+                     resp2 = 0,
+                     resp3 = 0;
                     while (cont < num) {
                         cont = cont + 1;
                         if (cont % 2 == 0) {
-                        a = 1;
-                        }else if (cont % 2 == 1){
-                        a = -1;
+                            a = 1;
+                        } else if (cont % 2 == 1) {
+                            a = -1;
                         }
-                        b = (2*cont) + 1;
+                        b = (2 * cont) + 1;
                         res = (a / b);
                         resp = res + resp;
-                        
+
                     }
-                    resp2 = resp*4;
+                    resp2 = resp * 4;
                     resp3 = resp2 + 4;
                     System.out.println("La aproximación es: " + resp3);
-                    
+
                     break;
 
                 case 3:
+                    entrada = new Scanner(System.in);
                     System.out.println("Ingrese el tamaño de la figura: ");
                     int tam = entrada.nextInt();
-                    for(i=1; tam==2;i++);
+                    System.out.println(" ");
+                    for (i = 1; i <= tam; i++) {
+                        for (int j = tam - i; j > 0; j--) {
+                            System.out.print(" ");
+                        }// que hace el for  se toma 
+                        for (int j = 1; j < i; j++) {
+                            System.out.print("**");
+                        }// usar el doble asterisco porque funciona mejor que multiplicar
+                        System.out.println("*");
+                    }
+                    for (i = 0; i <= tam; i++) {
+                        for (double x = 1; x < (2 * tam); x++) {
+                            System.out.print("+");
+                        }
+                        System.out.println("");
+                    }
+
                     break;
-                    
-                    
+
             }
+        }
     }
-}
 }
